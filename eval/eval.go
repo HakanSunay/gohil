aa464034589c2a682c109857a2905b0dcb8a7c2f
hakansunay <hakansunay@abv.bg>
2020-06-29 10:30:43 +0300

    Add acceptance tests for evaluating integer literals only

---
+++ eval/eval.go
@@ -0,0 +1,11 @@
+package eval
+
+import (
+	"github.com/HakanSunay/gohil/object"
+	"github.com/HakanSunay/gohil/syntaxtree"
+)
+
+func Eval(node syntaxtree.Node) object.Object {
+
+	return nil
+}
---
+++ eval/eval_test.go
@@ -0,0 +1,41 @@
+package eval
+
+import (
+	"github.com/HakanSunay/gohil/lexer"
+	"github.com/HakanSunay/gohil/object"
+	"github.com/HakanSunay/gohil/parser"
+	"testing"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input string
+		output int
+	}{
+		{"0", 0},
+		{"6", 6},
+		{"1337", 1337},
+	}
+	for _, tt := range tests {
+		evaluatedObj := evaluate(tt.input)
+		verifyIntegerObj(t, evaluatedObj, tt.output)
+	}
+}
+
+func evaluate(input string) object.Object {
+	l := lexer.NewLexer(input)
+	p := parser.NewParser(l)
+	program := p.ParseProgram()
+	obj := Eval(program)
+	return obj
+}
+
+func verifyIntegerObj(t *testing.T, obj object.Object, expected int) {
+	val, ok := obj.(*object.Integer)
+	if !ok {
+		t.Fatalf("Expected Integer object type, but got %T", obj)
+	}
+	if val.Value != expected {
+		t.Errorf("expected %v, but got %v", expected, val.Value)
+	}
+}
