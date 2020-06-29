d4a2e38b88f7a55ef337ae71512c71bc0e8e32ba
hakansunay <hakansunay@abv.bg>
2020-06-29 10:37:59 +0300

    Add plain single input integer evaluation

--- eval/eval.go
+++ eval/eval.go
@@ -6,6 +6,28 @@ import (
 )
 
 func Eval(node syntaxtree.Node) object.Object {
+	switch node := node.(type) {
+	// Statements:
+	case *syntaxtree.Program:
+		return evalStatements(node.Statements) // start traversing the program tree
+	case *syntaxtree.ExpressionStmt:
+		return Eval(node.Expression)
+
+	// Expressions
+	case *syntaxtree.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
 
 	return nil
+}
+
+func evalStatements(statements []syntaxtree.Stmt) object.Object {
+	var result object.Object
+
+	for _, stmt := range statements {
+		// last evaluated statement will end up as the result
+		result = Eval(stmt)
+	}
+
+	return result
 }
