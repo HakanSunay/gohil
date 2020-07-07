18ec5938636bd4faf8235b7a9089662b37a0bda2
hakansunay <hakansunay@abv.bg>
2020-07-07 22:46:59 +0300

    Evaluation function calls

--- eval/eval.go
+++ eval/eval.go
@@ -54,15 +54,23 @@ func Eval(node syntaxtree.Node, environment *object.Environment) object.Object {
 		if isError(left) {
 			return left
 		}
-
 		right := Eval(node.Right, environment)
 		if isError(right) {
 			return right
 		}
-
 		return evalInfixExpression(node.Operator, left, right)
 	case *syntaxtree.IfExpr:
 		return evalIfExpression(node, environment)
+	case *syntaxtree.CallExpr:
+		function := Eval(node.Function, environment)
+		if isError(function) {
+			return function
+		}
+		args := evalExpressions(node.Arguments, environment)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
+		return applyFunction(function, args)
 	case *syntaxtree.FunctionLiteral:
 		params := node.Parameters
 		body := node.Body
@@ -74,6 +82,23 @@ func Eval(node syntaxtree.Node, environment *object.Environment) object.Object {
 	}
 
 	return nil
+}
+
+func evalExpressions(exprs []syntaxtree.Expr, env *object.Environment) []object.Object {
+	var result []object.Object
+
+	// also evaluation from LEFT to RIGHT
+	for _, e := range exprs {
+		evaluated := Eval(e, env)
+		if isError(evaluated) {
+			// this ensures the error check for len 1
+			return []object.Object{evaluated}
+		}
+
+		result = append(result, evaluated)
+	}
+
+	return result
 }
 
 func evalProgram(statements []syntaxtree.Stmt, environment *object.Environment) object.Object {
@@ -236,4 +261,30 @@ func isError(obj object.Object) bool {
 		return obj.Type() == object.ErrorObject
 	}
 	return false
+}
+
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+	function, ok := fn.(*object.Function)
+	if !ok {
+		return newError("not a function: %s", fn.Type())
+	}
+
+	extendedEnv := extendFunctionEnv(function, args)
+	evaluated := Eval(function.Body, extendedEnv)
+	return unwrapReturnValue(evaluated)
+}
+
+func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
+	env := object.NewEnclosedEnvironment(fn.Env)
+	for paramIdx, param := range fn.Parameters {
+		env.Set(param.Value, args[paramIdx])
+	}
+	return env
+}
+
+func unwrapReturnValue(obj object.Object) object.Object {
+	if returnValue, ok := obj.(*object.ReturnValue); ok {
+		return returnValue.Value
+	}
+	return obj
 }
